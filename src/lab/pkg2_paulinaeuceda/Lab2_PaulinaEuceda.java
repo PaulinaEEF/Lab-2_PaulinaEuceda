dde1be8337d4870f4c41a3eabbe5b681e14f65fe
PaulinaEEF <60455386+PaulinaEEF@users.noreply.github.com>
2020-01-31 14:42:26 -0600

    Se creo la clase Personaje y crea personajes

--- src/lab/pkg2_paulinaeuceda/Lab2_PaulinaEuceda.java
+++ src/lab/pkg2_paulinaeuceda/Lab2_PaulinaEuceda.java
@@ -1,21 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab.pkg2_paulinaeuceda;
 
-/**
- *
- * @author Nohelia
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab2_PaulinaEuceda {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList<Personaje> lista = new ArrayList();
+        lista.add(new Personaje("mago","Plutarc","elfo",1.90,190,1800,"Le gusta el chocolate","zebes"));
+        lista.add(new Personaje("clerigo","Sam","enano",1.50,90,300,"Vive para las peleas","crateria"));
+        lista.add(new Personaje("barbaro","Gustavo","mediano",1.60,130,90,"Disfruta limpiar las armas","norfair"));
+        lista.add(new Personaje("picaro","Hugo","humano",1.70,160,35,"Tiene aguante","maridia"));
+        
+        Scanner lea = new Scanner(System.in);
+        char resp = 's';
+        while (resp == 's') {
+            System.out.println("1) Creacion de personajes");
+            System.out.println("2) Modificar personajes");
+            System.out.println("3) Ver atributos de un personaje");
+            System.out.println("4) Eliminar personajes");
+            System.out.println("5) Combate");
+            System.out.println("6) Salir");
+            System.out.print("Ingrese la opcion que desea: ");
+            int opcion = lea.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese la clase [clerigo, barbaro, mago, picaro]: ");
+                    String clase = lea.next();
+                    
+                    System.out.print("Ingrese el nombre: ");
+                    String nombre = lea.next();
+                    
+                    System.out.print("Ingrese la raza [mediano, enano, elfo, humano]: ");
+                    String raza = lea.next();
+                    
+                    System.out.print("Ingrese la estatura: ");
+                    double estatura = lea.nextDouble();
+                    
+                    System.out.print("Ingrese el peso: ");
+                    int peso = lea.nextInt();
+                    
+                    System.out.print("Ingrese el edad: ");
+                    int edad = lea.nextInt();
+                    
+                    System.out.print("Ingrese la descripcion: ");
+                    String descripcion = lea.nextLine();
+                    descripcion = lea.nextLine();   
+                    
+                    System.out.print("Ingrese la nacionalidad [norfair, brinstar, maridia, zebes, crateria]: ");
+                    String nacionalidad = lea.next();
+                    
+                    lista.add(new Personaje(clase, nombre, raza, estatura, peso, edad, descripcion, nacionalidad));
+                    
+                    System.out.println(lista);
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    resp='n';
+                    break;
+                default:
+                    break;
+            }
+
+        }
     }
-    
+
 }
---
+++ src/lab/pkg2_paulinaeuceda/Personaje.java
@@ -0,0 +1,110 @@
+
+package lab.pkg2_paulinaeuceda;
+
+
+public class Personaje {
+    private String clase;
+    private String nombre;
+    private String raza;
+    private double estatura;
+    private int peso;
+    private int años;
+    private String descripcion;
+    private String nacionalidad;
+
+    public Personaje() {
+    }
+    
+    
+
+    public Personaje(String clase, String nombre, String raza, double estatura, int peso, int años, String descripcion, String nacionalidad) {
+        setClase(clase);
+        this.nombre = nombre;
+        setRaza(raza);
+        this.estatura = estatura;
+        this.peso = peso;
+        this.años = años;
+        this.descripcion = descripcion;
+        setNacionalidad(nacionalidad);
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        if(clase.equals("clerigo") || clase.equals("barbaro") || clase.equals("mago") || clase.equals("picaro")){
+            this.clase = clase;
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        if(raza.equals("mediano") || raza.equals("enano") || raza.equals("elfo") || raza.equals("humano")){
+            this.raza = raza;
+        }
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getAños() {
+        return años;
+    }
+
+    public void setAños(int años) {
+        this.años = años;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        if(nacionalidad.equals("norfair") || nacionalidad.equals("brinstar") || nacionalidad.equals("maridia") || nacionalidad.equals("zebes") || nacionalidad.equals("crateria")){
+            this.nacionalidad = nacionalidad;
+            
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Personaje{" + "clase=" + clase + ", nombre=" + nombre + ", raza=" + raza + ", estatura=" + estatura + ", peso=" + peso + ", a\u00f1os=" + años + ", descripcion=" + descripcion + ", nacionalidad=" + nacionalidad + '}';
+    }
+    
+    
+    
+    
+}
